6a46200aefe20b0c2df9f719331bd7155a22c7e0
lkinash <kinash.lindsey@gmail.com>
2015-11-12 12:40:16 -0600

    Update Mode.java

--- git/SmartWristband/src/Mode.java
+++ git/SmartWristband/src/Mode.java
@@ -1,9 +1,74 @@
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import java.awt.event.*;
 
-public class Mode{
+public class Mode extends JFrame{
 
 	private int ledColour;
 	private char mode;
 	private boolean ledSecondaryOnOff;
+	
+	public Mode()
+	{
+		final JButton upButton = new JButton("UP");
+		final JButton downButton = new JButton("DOWN");
+		final JButton plusButton = new JButton("PLUS");
+		final JButton minusButton = new JButton("MINUS");
+		final JButton modeButton = new JButton("MODE");
+		
+		upButton.setEnabled(true);
+		downButton.setEnabled(true);
+		plusButton.setEnabled(true);
+		minusButton.setEnabled(true);
+		modeButton.setEnabled(true);
+		
+		upButton.addActionListener(new ActionListener(){		
+			
+			public void actionPerformed(ActionEvent e){
+			
+				buttonUpPress();
+			}
+		} );
+		
+		downButton.addActionListener(new ActionListener(){		
+			
+			public void actionPerformed(ActionEvent e){
+			
+				buttonDownPress();
+				}
+			} );
+
+		plusButton.addActionListener(new ActionListener(){		
+	
+			public void actionPerformed(ActionEvent e){
+	
+				buttonPlusPress();
+				}
+			} );
+		
+		minusButton.addActionListener(new ActionListener(){		
+	
+			public void actionPerformed(ActionEvent e){
+	
+				buttonMinusPress();
+				}
+			} );
+		
+		modeButton.addActionListener(new ActionListener(){		
+			
+			public void actionPerformed(ActionEvent e){
+	
+				buttonModePress();
+				}
+			} );
+		
+		getContentPane().add(upButton);
+		getContentPane().add(downButton);
+		getContentPane().add(plusButton);
+		getContentPane().add(minusButton);
+		getContentPane().add(modeButton);
+	}
 	
 	public void setLedColour(int colour)
 	{
